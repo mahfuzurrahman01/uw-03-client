53607869b71a165df63c8b5aaae4ed57e3f9ca85
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-02-03 11:42:51 +0600

    re adjust

--- preview.js
+++ preview.js
@@ -1,11 +1,24 @@
-const mouseTarget = document.getElementById('youtube');
+// declare the element
 
-mouseTarget.addEventListener('mouseenter', (e) => {
-    mouseTarget.setAttribute("src", "https://www.youtube.com/embed/jhq3ZOhgQL8?&fs=0&color=white&controls=0&disablekb=1&mute=1&autoplay=1")
+const div = document.getElementById('div')
+const video = document.querySelector("#div iframe")
+const img = document.querySelector("#div img")
+video.setAttribute('id', 'none')
 
-});
+// mouseenter event 
 
-mouseTarget.addEventListener('mouseout', (e) => {
-    mouseTarget.setAttribute('src', 'https://www.youtube.com/embed/jhq3ZOhgQL8?&fs=0&color=white&controls=0&disablekb=1')
+div.addEventListener('mouseenter', function () {
+    img.setAttribute('id', 'none')
+    video.removeAttribute('id')
+    video.setAttribute('src', "https://www.youtube.com/embed/jhq3ZOhgQL8?&fs=0&color=white&controls=0&disablekb=1&mute=1&autoplay=1")
+})
 
-});
+//mouse out
+
+div.addEventListener('mouseleave', function () {
+    video.setAttribute('src', "https://www.youtube.com/embed/jhq3ZOhgQL8?&fs=0&color=white&controls=0&disablekb=1")
+    video.setAttribute('id', 'none')
+    img.removeAttribute('id')
+})
+
+
